7a861e2bdf37d71901fc492f13a4c72893357ba3
diskup <taigaa.1998@gmail.com>
2021-10-23 00:25:14 +0000

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,21 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!,except: [:top]
   before_action :ensure_current_user, {only: [:edit, :update]}
-  
+
   def ensure_current_user
     @user = User.find(params[:id])
     redirect_to user_path(current_user.id) if @user != current_user
   end
-  
+
   def index
+    @book = Book.new
     @user = current_user
     @users = User.page(params[:page])
     @books = @user.books.page(params[:page])
   end
 
   def show
+    @book = Book.new
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page])
   end
@@ -31,13 +33,13 @@ class UsersController < ApplicationController
        render :edit
     end
   end
-  
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
     redirect_to users_path
   end
-  
+
 private
 
   def user_params
